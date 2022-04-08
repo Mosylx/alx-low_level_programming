dbb199e7b0ffe122e4f42445b7825cd6728abed1
mosylx <moselly.balep03@gmail.com>
2022-04-08 12:04:56 -0700

    always print script

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: ALways 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		char c = 'z';
+
+
+
+			for (; c >= 'a'; c--)
+
+						putchar(c);
+
+				putchar('\n');
+
+					return (0);
+
+}
