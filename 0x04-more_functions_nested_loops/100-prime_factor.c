bb8c387e6e10c888bf00bf70cfd91afc1ddd9712
mosylx <moselly.balep03@gmail.com>
2022-04-14 15:56:38 -0700

    More nested loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * Return: Null
+ */
+
+int main(void)
+{
+	long n, i;
+	n = 6128524755143;
+	for (i = 2; i < n; i++)
+	{
+	while (n % i == 0)
+		n = n / i;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * Return: Null
+ */
+
+int main(void)
+{
+	long n, i;
+	n = 6128524755143;
+	for (i = 2; i < n; i++)
+	{
+	while (n % i == 0)
+		n = n / i;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * Return: Null
+ */
+
+int main(void)
+{
+	long n, i;
+	n = 6128524755143;
+	for (i = 2; i < n; i++)
+	{
+	while (n % i == 0)
+		n = n / i;
+	}
+	printf("%lu\n", n);
+	return (0);
+}
+
