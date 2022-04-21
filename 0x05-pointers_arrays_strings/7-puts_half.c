c4d7dff8610328f18beaa8aeb5563a55785a92c2
mosylx <moselly.balep03@gmail.com>
2022-04-20 18:39:14 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * puts_half - print half string
+ * @str: string
+ *
+ * Return: Null
+ */
+
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		i++;
+	for (i /= 2; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
