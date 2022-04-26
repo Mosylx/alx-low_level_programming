75f5a1418f571c2ca875b71bc8aa8787f5bcdb81
mosylx <moselly.balep03@gmail.com>
2022-04-26 16:24:46 -0700

    More pointers

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,13 @@
 
 char *_strchr(char *s, char c);
 {
-	int index;
+	unsigned int i = 0;
 
-	for (index = 0; s[index] >= '\0'; index++)
-	{
-		if (s[index] == c)
-			return (s = index);
-	}
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s +i);
+	if (*(s + i) == c)
+		return (s + i);
 
 	return ('\0');
 }
