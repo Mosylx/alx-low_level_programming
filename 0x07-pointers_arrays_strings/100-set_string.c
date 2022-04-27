d7b00ab857cbe016d5663cb18e239bbf94e84d19
mosylx <moselly.balep03@gmail.com>
2022-04-27 15:15:55 -0700

    Arrays strings

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * ste_string - sets the value of a pointer toa char
+ * @s: source adress
+ * @to: target adress
+ *
+ * Return: None
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
